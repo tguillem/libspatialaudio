b20df396bf59de7b4f8796f475e900e706d92ffa
Thomas Guillem <thomas@gllm.fr>
2020-07-15 17:04:37 +0200

    Revert "WIP: no -lz on windows"
    This reverts commit 32e0a8fc084f913d3edebbd3ecb8bd3ca7203f4f.

--- cmake/spatialaudio.pc.cmake
+++ cmake/spatialaudio.pc.cmake
@@ -5,5 +5,5 @@ includedir=${prefix}/include
 Name: libspatialaudio
 Description: Spatial audio rendering library
 Version: @PACKAGE_VERSION_MAJOR@.@PACKAGE_VERSION_MINOR@.@PACKAGE_VERSION_PATCH@
-Libs: -L${libdir} -lspatialaudio @MYSOFA_LIB@ -lm
+Libs: -L${libdir} -lspatialaudio @MYSOFA_LIB@ -lm -lz
 Cflags: -I${includedir} @MYSOFA_INCLUDE@
